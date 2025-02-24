f58668036fb166364edd9e22041aed99a11b0476
Eliud Lucio <EliudJonathe@outlook.com>
2025-02-24 12:52:30 -0600

    Desarrollo ToolBar

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.todoapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import com.google.android.material.bottomappbar.BottomAppBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,10 @@ class MainActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         setContentView(R.layout.activity_main)
+
+        // Configurar Toolbar
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         // Declaramos variables de los objetos
         val etnum1 = findViewById<EditText>(R.id.num1)
@@ -36,5 +41,28 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // The Toolbar defined in the layout has the id "my_toolbar".
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+    }
+
+    // Inflar el menú en la Toolbar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.dropdown_menu -> {
+            // Código al darle click
+            Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        else -> {
+            // The user's action isn't recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
     }
 }
